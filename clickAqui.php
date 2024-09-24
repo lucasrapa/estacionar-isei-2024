538fca9c3bf16cec8375dc47d749554c36238d2d
lucas <lucasrapaport195@gmail.com>
2024-09-24 19:25:56 -0300

    -m "Commit 2409241927"

--- clickAqui.php
+++ clickAqui.php
@@ -1,3 +1,6 @@
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+<link rel="stylesheet" href="./assets/css/main.css">
+
 <div style="display: flex; justify-content: center;">
     <div style="background-color: #1450FF;width:80%;border-radius:35px;padding:50px">
         <div style="display: flex; justify-content: space-between;">
